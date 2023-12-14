41fc8a56b6583bc7cc7b6992e1d785ba2c707435
Darian T. Yang <dty7@pitt.edu>
2023-12-14 16:10:35 -0500

    added equilibrated run, set up basic file structure for WEER

---
+++ odld/weer.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+class WEER:
+    '''
+    Weighted ensemble with ensemble reweighting.
+    '''
+    def __init__(self):
+        '''
+        Parameters
+        ----------
+        '''
+        pass
