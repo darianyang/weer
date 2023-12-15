ef25e2d1a7b859e9fb48465beac385a59d32b796
Darian T. Yang <dty7@pitt.edu>
2023-12-15 16:44:54 -0500

    testing other methods of fixing mismatched array lengths, align supports and kde, not working so far

--- odld/weer.py
+++ odld/weer.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy.stats import entropy
 from scipy.special import rel_entr, kl_div
 from scipy.optimize import minimize
+from sklearn.neighbors import KernelDensity
 
 class WEER:
     '''
@@ -49,10 +50,27 @@ class WEER:
         hist, bin_edges = np.histogram(data, bins=bins, density=True, weights=weights)
         return hist
 
+    def align_support(self, true_distribution, simulated_distribution):
+        """
+        Align the support of two distributions by adding zero probabilities where needed.
+        """
+        unique_values = np.union1d(true_distribution, simulated_distribution)
+        true_distribution_aligned = np.zeros_like(unique_values, dtype=float)
+        simulated_distribution_aligned = np.zeros_like(unique_values, dtype=float)
+
+        for i, value in enumerate(unique_values):
+            if value in true_distribution:
+                true_distribution_aligned[i] = true_distribution[true_distribution == value].sum()
+            if value in simulated_distribution:
+                simulated_distribution_aligned[i] = simulated_distribution[simulated_distribution == value].sum()
+
+        return true_distribution_aligned, simulated_distribution_aligned
+
     def kl_divergence(self, weights, true_distribution, simulated_distribution, 
                       bins=100, epsilon=1e-10):
         """
         Calculate KL divergence between two distributions using binning.
+        # TODO: binning is best here? how to account for alternate array lengths?
 
         Parameters
         ----------
@@ -63,14 +81,20 @@ class WEER:
             Small constant to avoid zero division error
         """
         # only need to weight simulated data from WE
-        true_hist = self.bin_data(true_distribution, bins)
-        simulated_hist = self.bin_data(simulated_distribution, bins, weights)
+        # TODO: wait, I already weighted it during pdist generation
+        # true_hist = self.bin_data(true_distribution, bins)
+        # #simulated_hist = self.bin_data(simulated_distribution, bins, weights)
+        # simulated_hist = self.bin_data(simulated_distribution, bins)
 
-        # Add a small constant epsilon to avoid division by zero
-        true_hist += epsilon
-        simulated_hist += epsilon
+        ## Add a small constant epsilon to avoid division by zero
+        #true_hist += epsilon
+        #simulated_hist += epsilon
 
-        return entropy(true_hist, simulated_hist)
+        # testing out the alignment instead
+        true_distribution, simulated_distribution = \
+            self.align_support(true_distribution, simulated_distribution)
+
+        return entropy(true_distribution, simulated_distribution)
 
     def optimize_weights(self, true_distribution, initial_weights, simulated_distribution):
         """
@@ -96,40 +120,74 @@ class WEER:
         else:
             raise ValueError("Optimization failed.")
 
+    def make_pdist(self, pcoord, weights, bins=100):
+        '''
+        Make a 1D pdist in units of kT.
 
-def make_pdist(pcoord, weights, bins=100):
-    '''
-    Make a 1D pdist in units of kT.
+        Parameters
+        ----------
+        pcoord : array
+        weights : array
+        bins : int
+        '''
+        # make an 1D array to fit the hist values based off of bin count
+        histogram = np.zeros((bins))
 
-    Parameters
-    ----------
-    pcoord : array
-    weights : array
-    bins : int
-    '''
-    # make an 1D array to fit the hist values based off of bin count
-    histogram = np.zeros((bins))
+        # precalculate histrange
+        histrange = (np.min(pcoord), np.max(pcoord))
 
-    # loop each segment in the current iteration
-    for seg in range(0, pcoord.shape[0]):
-        counts, bins = np.histogram(pcoord[seg], bins=bins,
-                                    range=(np.min(pcoord), np.max(pcoord)))
+        # loop each segment in the current iteration
+        for seg in range(0, pcoord.shape[0]):
+            counts, bins = np.histogram(pcoord[seg], bins=bins,
+                                        range=histrange)
 
-        # multiply counts vector by weight scalar from weight array
-        counts = np.multiply(counts, weights[seg])
+            # multiply counts vector by weight scalar from weight array
+            counts = np.multiply(counts, weights[seg])
 
-        # add all of the weighted walkers to total array for the 
-        # resulting linear combination
-        histogram = np.add(histogram, counts)
+            # add all of the weighted walkers to total array for the 
+            # resulting linear combination
+            histogram = np.add(histogram, counts)
 
-    # get bin midpoints
-    midpoints_x = (bins[:-1] + bins[1:]) / 2
+        # get bin midpoints
+        midpoints_x = (bins[:-1] + bins[1:]) / 2
+        
+        # normalize hist to kT
+        histogram = -np.log(histogram / np.max(histogram))
+
+        return midpoints_x, histogram
+
+    def run_weer(self):
+        '''
+        Main public class method for WEER.
+        '''
+        # make simulated dist pdist
+        x, simulated_dist = self.make_pdist(self.pcoords, self.weights)
+
+        # TODO: make sure they both sum to 1
+
+        # calc KL divergence from true dist
+        #kld = self.kl_divergence(self.weights, self.true_dist[:,1], simulated_dist)
+        print(self.true_dist[:,1].shape, simulated_dist[4:].shape)
+        kld = entropy(self.true_dist[:,1], simulated_dist[4:])
+
+        print(kld)
+
+def plot_kde(data):
+    """
+    Calc the KDE of the given data.
+    TODO: replace the bins with KDE?
+    """
+    # You can adjust the bandwidth parameter
+    kde = KernelDensity(bandwidth=0.6)
+    kde.fit(data[:, np.newaxis])
     
-    # normalize hist to kT
-    histogram = -np.log(histogram / np.max(histogram))
-
-    return midpoints_x, histogram
-
+    # TODO: update to bin midpoint x values?
+    #       need to think about the theory for is this makes sense
+    x_vals = np.linspace(min(data), max(data), 1000)
+    log_dens = kde.score_samples(x_vals[:, np.newaxis])
+    dens = np.exp(log_dens)
+    
+    return x_vals, dens
 
 if __name__ == "__main__":
     # test data (1D array of 1D ODLD endpoints)
@@ -145,21 +203,22 @@ if __name__ == "__main__":
     #print(pcoords.reshape(-1))
     #hist, bin_edges = np.histogram(pcoords.reshape(-1), bins=100)
     # TODO: NEXT, make the pdist, use this to compare to true_dist
-    x, hist = make_pdist(pcoords, weights)
-    plt.plot(x, hist)
-    plt.show()
+    #x, hist = make_pdist(pcoords, weights) # turn into static method?
+    #plt.plot(x, hist)
+    #plt.show()
 
     # TODO: note that I'm comparing the ODLD potential which
     #       is already in same units as kT from WE
     #       but for future, e.g. EPR distance, I would need
     #       to probably normalize dists to 1, but KL div already
     #       needs this done anyway... so should be fine
-    #true_dist = np.loadtxt("true_1d_odld.txt")
+    true_dist = np.loadtxt("true_1d_odld.txt")
     #plt.plot(true_dist[:,0], true_dist[:,1])
     #plt.show()
 
     # this is the full pcoord array, in this case (80, 5, 2)
     # for 80 walkers, 5 frames of simulation each, and 2 pcoords (X and Y)
+    # TODO: be able to also handle multi-dim pcoord, but only compare dim 0
     #pcoords = np.load('pcoords_full.npy')
 
     # test init data
@@ -167,7 +226,8 @@ if __name__ == "__main__":
     # weights = np.array([0.02] * 50)
 
     # WEER test
-    #reweight = WEER()
+    reweight = WEER(pcoords, weights, true_dist)
+    reweight.run_weer()
 
     # KL divergence test
     #p = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
