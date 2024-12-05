889d8d62b5b939b74c99920bc93db5fa288a5509
Darian T. Yang <dty7@pitt.edu>
2024-12-05 16:48:46 +0100

    calculated S2 from ired

--- relaxation/ired.cpp
+++ relaxation/ired.cpp
@@ -12,4 +12,4 @@ matrix ired name matired order 2
 diagmatrix matired vecs 1 out ired.vec name ired.vec
 
 # Calculate R1 and R2 relaxation rates and NOE
-ired relax NHdist 1.02 freq 600.13 tstep 10 tcorr 5000.0 out ired.out noefile ired.noe order 2 modes ired.vec
+ired relax NHdist 1.02 freq 600.13 tstep 10 tcorr 5000.0 out ired.out noefile ired.noe order 2 modes ired.vec orderparamfile ired.s2
