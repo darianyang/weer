1b802500daa8a352493ff659ac422b179cc50000
Darian T. Yang <dty7@pitt.edu>
2024-12-12 16:46:03 +0100

    ran ired on both NH bond vectors of ala dipep to get reference data for rw

--- relaxation/ired.cpp
+++ relaxation/ired.cpp
@@ -4,12 +4,13 @@ trajin alanine-dipeptide-0-250ns.xtc
 
 # Define the NH vectors for analysis
 vector vALA @7 ired @8
+vector vNMR @17 ired @18
 
 # iRED matrix 
 matrix ired name matired order 2
 
 # Diagonalize IRED matrix
-diagmatrix matired vecs 1 out ired.vec name ired.vec
+diagmatrix matired vecs 2 out ired.vec name ired.vec
 
 # Calculate R1 and R2 relaxation rates and NOE
 ired relax NHdist 1.02 freq 600.13 tstep 10 tcorr 5000.0 out ired.out noefile ired.noe order 2 modes ired.vec orderparamfile ired.s2
--- relaxation/reweight_test.py
+++ relaxation/reweight_test.py
@@ -4,4 +4,21 @@ Testing relaxation calc and reweighting.
 
 import absurder
 import numpy as np
+import matplotlib.pyplot as plt
 
+# first try with example data
+# then try with calc relaxation data from script
+rex = 'data/same_ff/nmr.npy'
+rmd = 'data/same_ff/md.npy'
+
+#rw = absurder.ABSURDer(rex, rmd)
+
+#rw.plot_comparison(0)
+
+# rw.reweight(0)
+
+# for i in range(3):
+#     rw.plot_phix2r( i )
+
+# opt_theta = 100
+# rw.plot_comparison( 0, opt_theta )
